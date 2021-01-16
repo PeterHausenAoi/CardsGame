b62668daec97b65d3f59fddd258151eb4dd1b7ab
Gergely Váraljai <varaljai.gergely@gmail.com>
2021-01-16 18:37:10 +0100

    spring support added. closes #1

---
+++ src/test/java/com/github/PeterHausenAoi/CardsGame/CardsGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.PeterHausenAoi.CardsGame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardsGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

662def3c86e37a6bcbe0cbbc299d0e0a97c7dee9
Gergely Váraljai <varaljai.gergely@gmail.com>
2021-01-17 22:53:50 +0100

    spring app docker file added. closes #21

--- initScripts/db/init_db.sh
+++ initScripts/db/init_db.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 docker build -t cards_game_db .
-docker run -it --name cardsGame --rm -p 4999:5432 -e POSTGRES_PASSWORD=pgis -d cards_game_db
+docker run -it --name cards_game_db --rm -p 4999:5432 -e POSTGRES_PASSWORD=pgis -d cards_game_db
---
+++ init_spring_app.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+docker build -t cards_game_app .
+docker run --net=host -it --name cards_game_spring_app --rm -p 8080:8080 -d cards_game_app
+docker logs cards_game_spring_app -f
